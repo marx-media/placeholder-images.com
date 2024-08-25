fba752b2e345c9662bf7d95f7ea8e66a61056ff8
Dominic Marx <dmarx@marxulm.de>
2024-08-25 23:52:13 +0200

    wip

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -51,6 +51,19 @@ export const useAuth = () => {
     return errors
   }
 
+  const linkOrSignIn = async (result: Promise<UserCredential>) => {
+    try {
+      return await result
+    } catch (error: any) {
+      if (error.code === 'auth/credential-already-in-use') {
+        const authCredentials = OAuthProvider.credentialFromError(error)
+        if (authCredentials)
+          return await signInWithCredential(auth, authCredentials)
+      }
+      throw error
+    }
+  }
+
   const getProviders = () => [
     {
       label: 'Continue with Google',
@@ -58,37 +71,14 @@ export const useAuth = () => {
       color: 'gray' as const,
       click: async (): Promise<UserCredential> => {
         const provider = new GoogleAuthProvider()
-        try {
-          return await linkWithPopup(userCopy, provider)
-        } catch (error: any) {
-          if (error.code === 'auth/credential-already-in-use') {
-            const authCredentials = OAuthProvider.credentialFromError(error)
-            if (authCredentials)
-              return await signInWithCredential(auth, authCredentials)
-          }
-          throw error
-        }
+        return await linkOrSignIn(linkWithPopup(userCopy, provider))
       }
     },
   ]
 
   const signUpWithEmailAndPassword = async (name: string, email: string, password: string): Promise<UserCredential> => {
-    try {
-      const userCredentils = EmailAuthProvider.credential(email, password)
-      try {
-        return await linkWithCredential(userCopy, userCredentils)
-      } catch (error: any) {
-        if (error.code === 'auth/credential-already-in-use') {
-          const authCredentials = OAuthProvider.credentialFromError(error)
-          if (authCredentials)
-            return await signInWithCredential(auth, authCredentials)
-        }
-        throw error
-      }
-    } catch (error: any) {
-      console.log(error)
-      throw error
-    }
+    const userCredentils = EmailAuthProvider.credential(email, password)
+    return await linkOrSignIn(linkWithCredential(userCopy, userCredentils))
   }
 
   return {
